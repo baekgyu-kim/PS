9f1907987280209a1d4cc8231fa31d64bc63ae56
Baekgyu <centneuf0109@gmail.com>
2023-08-28 17:51:53 +0900

    2559

--- lecture1/h_2559.cpp
+++ lecture1/h_2559.cpp
@@ -1,3 +1,32 @@
+// 수열
+
 #include <bits/stdc++.h>
 
-int main() { return 0; }
+int main() {
+    int N, k = 0;
+    std::cin >> N;
+    std::cin >> k;
+    std::vector<int> temperature;
+    for (int i = 0; i < N; i++) {
+        int temp;
+        std::cin >> temp;
+        temperature.push_back(temp);
+    }
+
+    std::vector<int> prefix_sum;
+    prefix_sum.push_back(temperature[0]);
+    for (int i = 1; i < temperature.size(); i++) {
+        prefix_sum.push_back(prefix_sum[i - 1] + temperature[i]);
+    }
+
+    std::vector<int> k_sum;
+    k_sum.push_back(prefix_sum[k - 1]);
+    for (int i = 1; i < temperature.size() - k + 1; i++) {
+        int temp = prefix_sum[i + k - 1] - prefix_sum[i - 1];
+        k_sum.push_back(temp);
+    }
+
+    int max = *max_element(k_sum.begin(), k_sum.end());
+    std::cout << max;
+    return 0;
+}
