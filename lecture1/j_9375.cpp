23d55864cf658cdd10c9c99ea213c34e7ec5afc9
Baekgyu <centneuf0109@gmail.com>
2023-09-02 12:28:58 +0900

    9375

--- lecture1/j_9375.cpp
+++ lecture1/j_9375.cpp
@@ -1,3 +1,28 @@
-#include <bits/stdc++.h>
+// 패션왕 신해빈
 
-int main() { return 0; }
+#include <bits/stdc++.h>
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int test_case_num = 0;
+    std::cin >> test_case_num;
+
+    for (int i = 0; i < test_case_num; i++) {
+        int num = 0;
+        std::cin >> num;
+        std::map<std::string, int> map;
+        for (int i = 0; i < num; i++) {
+            std::string name, category;
+            std::cin >> name >> category;
+            map[category] += 1;
+        }
+        int result = 1;
+        for (std::pair<std::string, int> pair : map) {
+            result *= (pair.second + 1);
+        }
+        result -= 1;
+        std::cout << result << "\n";
+    }
+}
