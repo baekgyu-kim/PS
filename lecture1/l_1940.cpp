808adfd8e9c061723720dacfede1956aa6d4649d
Baekgyu <centneuf0109@gmail.com>
2023-09-07 10:51:35 +0900

    1940

--- lecture1/l_1940.cpp
+++ lecture1/l_1940.cpp
@@ -1,3 +1,30 @@
+// 주몽
+
 #include <bits/stdc++.h>
 
-int main() { return 0; }
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> numbers;
+    for (int i = 0; i < N; i++) {
+        int temp;
+        cin >> temp;
+        numbers.push_back(temp);
+    }
+    int result = 0;
+    if(N < 2){
+        cout << result;
+        return 0;
+    }
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = i + 1; j < N; j++) {
+            if (numbers[i] + numbers[j] == M) {
+                result += 1;
+            }
+        }
+    }
+    cout << result;
+    return 0;
+}
