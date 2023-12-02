d1ff2961286164b5a163e9bbdde79d37f6d3335b
Baekgyu <centneuf0109@gmail.com>
2023-12-02 12:20:56 +0900

    2828

--- lecture2/f_2828.cpp
+++ lecture2/f_2828.cpp
@@ -1,3 +1,34 @@
+// 사과 담기 게임
+
 #include <bits/stdc++.h>
 using namespace std;
-int main() { return 0; }
+
+int M, N, J = 0;
+
+int main() {
+    cin >> N >> M;
+    int left = 0;
+    int right = M - 1;
+    cin >> J;
+    int apple = 0;
+    int count = 0;
+    for (int i = 0; i < J; i++) {
+        cin >> apple;
+        apple -= 1;
+        if (left <= apple && apple <= right) {
+            continue;
+        } else if (apple < left) {
+            int move = left - apple;
+            count += move;
+            left -= move;
+            right -= move;
+        } else if (right < apple) {
+            int move = apple - right;
+            count += move;
+            left += move;
+            right += move;
+        }
+    }
+    cout << count;
+    return 0;
+}
