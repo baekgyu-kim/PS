a763b9785f70d43389f718328c8a81ef8e5217db
Baekgyu <centneuf0109@gmail.com>
2023-12-09 14:56:43 +0900

    9012

--- lecture2/n_9012.cpp
+++ lecture2/n_9012.cpp
@@ -1,3 +1,41 @@
+// 괄호
+
 #include <bits/stdc++.h>
 using namespace std;
-int main() { return 0; }
+
+int T = 0;
+vector<string> strings;
+
+string is_correct(string s) {
+    stack<char> st;
+    for (char c : s) {
+        if(c == '('){
+            st.push(c);
+        }else{
+            if(st.empty() == true){
+                return "NO";
+            }else{
+                st.pop();
+            }
+        }
+    }
+    if(st.empty() == true){
+        return "YES";
+    }else{
+        return "NO";
+    }
+}
+
+int main() {
+    cin >> T;
+    string s;
+    for (int i = 0; i < T; i++) {
+        cin >> s;
+        strings.push_back(s);
+    }
+    for (int i = 0; i < T; i++) {
+        s = strings[i];
+        cout << is_correct(s) << "\n";
+    }
+    return 0;
+}
