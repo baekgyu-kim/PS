24ed05090ee5b8acec1be5c5b215b23e0e128863
Baekgyu <baekgyu.dev@gmail.com>
2024-02-15 18:14:11 +0900

    1325

--- 추천문제/2/s_1325.cpp
+++ 추천문제/2/s_1325.cpp
@@ -1,3 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() { return 0; }
+int N, M = 0;
+vector<int> adj[10004];
+int visited[10004] = {0};
+int nodes[10004];
+
+bool cmp(int a, int b) { return a > b; }
+
+int dfs(int u) { 
+    visited[u] = 1;
+    int res = 1;
+    for(int v : adj[u]){
+        if(visited[v] == 1){
+            continue;
+        }
+        res += dfs(v);
+    }
+    return res;
+}
+
+int main() {
+    cin >> N >> M;
+    int to, from = 0;
+    for (int i = 0; i < M; i++) {
+        cin >> to >> from;
+        adj[from].push_back(to);
+    }
+    for (int node = 1; node <= N; node++){
+        memset(visited, 0, sizeof(visited));
+        int dfs_res = dfs(node);
+        nodes[node] = dfs_res;
+        M = max(dfs_res, M);
+    }
+    int M = *max_element(nodes + 1, nodes+ N + 1);
+    for (int node = 1; node <= N; node++) {
+        if(M == nodes[node]){
+            cout << node << " ";
+        }
+    }
+    return 0;
+}
