87ad637ceb132ca295d3c8f3030811b7a194f2bd
Baekgyu <baekgyu.dev@gmail.com>
2024-02-21 15:11:25 +0900

    9934

--- 추천문제/3/n_9934.cpp
+++ 추천문제/3/n_9934.cpp
@@ -1,3 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() { return 0; }
+int K = 0;
+vector<int> v;
+vector<int> res[15];
+
+void solve(int first, int last, int depth) {
+    if (first == last) {
+        res[depth].push_back(v[first]);
+        return;
+    }
+    int mid_index = (first + last) / 2;
+    res[depth].push_back(v[mid_index]);
+    solve(first, mid_index - 1, depth + 1);
+    solve(mid_index + 1, last, depth + 1);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> K;
+    int input;
+    for (int i = 1; i < (int)(pow(2, K)); i++) {
+        cin >> input;
+        v.push_back(input);
+    }
+    solve(0, v.size() - 1, 0);
+    int i = 0;
+    while(res[i].size() > 0){
+        for (int n : res[i]) {
+            cout << n << " ";
+        }
+        cout << "\n";
+        i++;
+    }
+
+    return 0;
+}
