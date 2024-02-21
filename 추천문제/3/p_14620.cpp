bfd77f10e4fa123daf4a73a7e639e92dc8cc6fd7
Baekgyu <baekgyu.dev@gmail.com>
2024-02-21 17:42:58 +0900

    14620

--- 추천문제/3/p_14620.cpp
+++ 추천문제/3/p_14620.cpp
@@ -1,3 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() { return 0; }
+int N = 0;
+int a[204][204] = {0};
+int visited[204][204] = {0};
+int sum[204][204];
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+int res = INT_MAX;
+
+bool is_ok(int y, int x) {
+    if (visited[y][x] == 1) {
+        return false;
+    }
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny < 0 || nx < 0 || ny >= N || nx >= N){
+            return false;
+        }
+        if (visited[ny][nx] == 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int add_flower_and_return_price(int y, int x) {
+    visited[y][x] = 1;
+    int sum = a[y][x];
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        visited[ny][nx] = 1;
+        sum += a[ny][nx];
+    }
+    return sum;
+}
+
+void erase_flower(int y, int x) { 
+    visited[y][x] = 0;
+    for (int i = 0; i < 4; i++){
+        visited[y + dy[i]][x + dx[i]] = 0;
+    }
+}
+
+void solve(int nth_flower, int sum){
+    if (nth_flower == 3) {
+        res = min(res, sum);
+        return;
+    }
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            if(!is_ok(i, j)){
+                continue;
+            }
+            int nth_price = add_flower_and_return_price(i, j);
+            solve(nth_flower + 1, sum + nth_price);
+            erase_flower(i, j);
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> a[i][j];
+        }
+    }
+    solve(0, 0);
+    cout << res;
+    return 0;
+}
