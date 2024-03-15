950d5e84ca1a933ec699c8e0c17a1f5969f6ed9f
Baekgyu <baekgyu.dev@gmail.com>
2024-03-15 12:29:30 +0900

    1094

--- 백준/추천문제/4/g_1094.cpp
+++ 백준/추천문제/4/g_1094.cpp
@@ -1,3 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() { return 0; }
+int X = 0;
+int res = 1;
+
+void input() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> X;
+}
+
+int main() {
+    input();
+    while (X > 1) {
+        if (X & 1){
+            res++;
+        }
+        X /= 2;
+    }
+    cout << res;
+    return 0;
+}
