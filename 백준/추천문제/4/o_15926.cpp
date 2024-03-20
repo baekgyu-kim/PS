0cae150f457896daa31eb3ea77672ebe2b05cb14
Baekgyu <baekgyu.dev@gmail.com>
2024-03-20 20:02:03 +0900

    15926

--- 백준/추천문제/4/o_15926.cpp
+++ 백준/추천문제/4/o_15926.cpp
@@ -1,3 +1,35 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-int main() { return 0; }
+
+int N, _max;
+string s;
+stack<int> st;
+
+void input(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N;
+    cin >> s;
+}
+
+int main() {
+    input();
+    st.push(-1);
+    for (int i = 0; i < N; i++) {
+        if (s[i] == '(')
+            st.push(i);
+        if (s[i] == ')') {
+            st.pop();
+            if (!st.empty()) {
+                _max = max(_max, i - st.top());
+            } else {
+                st.push(i);
+            }
+        }
+    }
+    cout << _max << '\n';
+
+    return 0;
+}
